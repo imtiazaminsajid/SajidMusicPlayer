08615071284a12fbed458c9b682a2dc2df9f56b9
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-09-22 11:46:22 +0600

    Data Model

--- app/src/main/java/com/example/imtiazaminsajid/sajidmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/sajidmusicplayer/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.imtiazaminsajid.sajidmusicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  ListView songView;
+    private ArrayList<Song> songList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songView = findViewById(R.id.song_list);
+
+        songList = new ArrayList<Song>();
+    }
+
+    public void getSongList(){
+        
     }
 }
